87a71cff58d7a930ba3e53e030c221a064601708
Miguel <mbetanc2.students.kennesaw.edu>
2015-11-05 17:46:30 -0500

    SoftwareProject Class must be fully completed in detail, home_page.fxml must be completed and general cleaning must be completed.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,6 @@ package main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -12,12 +11,8 @@ import java.io.IOException;
 public class Main extends Application{
 
     public Stage stage;
+
     public HomeController HC;
-    Button projectViewButton;
-
-    Scene projectViewScene;
-
-
 
     @Override
     public void start(Stage primaryStage) throws IOException
@@ -27,17 +22,8 @@ public class Main extends Application{
         primaryStage.setScene(homeScene());
         primaryStage.setMaxWidth(600);
         primaryStage.setMaxHeight(400);
-
-
         primaryStage.show();
-
         HC = new HomeController();
-      /*  if (HC.viewProjectButton.isPressed())
-        {
-            primaryStage.setScene(projectViewScene());
-        }*/
-
-
     }
 
     public Scene homeScene () throws IOException
@@ -46,44 +32,8 @@ public class Main extends Application{
         return new Scene(root);
     }
 
-    public void projectViewScene () throws IOException
+    public static void main(String[] args)
     {
-        Pane root = FXMLLoader.load(getClass().getResource("page_project_view.fxml"));
-        stage.setScene(new Scene(root));
-    }
-
-    public static void main(String[] args) {
         launch(args);
     }
-
-
-
 }
-/*
-
-primaryStage.setTitle("PMT 3000");
-FXMLLoader homeLoader = new FXMLLoader(getClass().getResource("../res/page_home.fxml"));
-Parent root = FXMLLoader.load(getClass().getResource("../res/page_home.fxml"));
-primaryStage.setScene(mainScreen);
-primaryStage.show();
-*/
-
-/*
-         stage = primaryStage;
-        primaryStage.setTitle("PMT 3000");
-
-        FXMLLoader homeLoader = new FXMLLoader(getClass().getResource("../res/page_home.fxml"));
-        //Parent root = FXMLLoader.load(getClass().getResource("../res/page_home.fxml"));
-
-        Pane homePane = homeLoader.load();
-
-        HomeController homeController = homeLoader.getController();
-
-        homeController.setPrevStage(primaryStage);
-
-       // Parent projectViewSc = FXMLLoader.load(getClass().getResource("../res/page_project_view.fxml"));
-        Scene mainScreen = new Scene(homePane);
-
-        primaryStage.setScene(mainScreen);
-        primaryStage.show();
- */
