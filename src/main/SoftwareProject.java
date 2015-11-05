ba71ae05a51e59dc2f1a56d5a5a5367a2f1ef066
Miguel <mbetanc2.students.kennesaw.edu>
2015-11-05 17:56:36 -0500

    SoftwareProject Class must be fully completed in detail, home_page.fxml must be completed and general cleaning must be completed.

--- src/main/SoftwareProject.java
+++ src/main/SoftwareProject.java
@@ -3,9 +3,11 @@ package main;
 public class SoftwareProject {
     public String projectName;
     public String projectManager;
+    public Integer hoursWorked;
 
-    public SoftwareProject(String projectName, String projectManager) {
+    public SoftwareProject(String projectName, String projectManager, Integer hoursWorked) {
         this.projectName = projectName;
         this.projectManager = projectManager;
+        this.hoursWorked = hoursWorked;
     }
 }
